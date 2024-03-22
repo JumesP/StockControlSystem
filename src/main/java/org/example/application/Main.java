ee51951b2955656bb0d2c971267ef8dbf85bc337
JumesP <118614403+JumesP@users.noreply.github.com>
2024-03-22 23:49:04 +0000

    minor changes

--- src/main/java/org/example/application/Main.java
+++ src/main/java/org/example/application/Main.java
@@ -5,10 +5,14 @@ import javafx.geometry.*;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.*;
 import javafx.scene.text.*;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
+
+import javax.swing.plaf.ProgressBarUI;
 
 public class Main extends Application {
     @Override
@@ -19,6 +23,14 @@ public class Main extends Application {
             Scene scene = new Scene(root, 400, 400, Color.BLACK);
 //            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
             primaryStage.setTitle("Supermarket Stock Management System");
+
+            // Logo
+          //  Image icon = new Image("/main/java/org/example/application/icon.png");
+          //  primaryStage.getIcons().add(icon);
+
+            primaryStage.setFullScreen(true);
+            primaryStage.setFullScreenExitHint("YOU CAN'T EXIT FULLSCREEN MODE");
+            primaryStage.setFullScreenExitKeyCombination(KeyCombination.valueOf("Ctrl+X"));
 
             primaryStage.setScene(scene);
             primaryStage.show();
