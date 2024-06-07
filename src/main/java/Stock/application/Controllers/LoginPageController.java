b0c2ced339c04065b9a991d6df0b17d0e6cb8719
JumesP <118614403+JumesP@users.noreply.github.com>
2024-06-07 23:43:58 +0100

    login partial functionalty

--- src/main/java/Stock/application/Controllers/LoginPageController.java
+++ src/main/java/Stock/application/Controllers/LoginPageController.java
@@ -1,13 +1,32 @@
-package Stock.application.Controllers.Login;
+package Stock.application.Controllers;
 
+import Stock.application.Models.LoginPageModel;
 import javafx.scene.Node;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class LoginPageController {
+    LoginPageModel loginPageModel = new LoginPageModel();
+//    LoginPageModel loginPageModel = new LoginPageModel();
+
+    public TextField Username;
+    public PasswordField Password;
+    public Button submit;
+
+    public void login(ActionEvent event) {
+        if (loginPageModel.isLogin(Username.getText(), Password.getText())) {
+            System.out.println("Login successful");
+        } else {
+            System.out.println("Login failed");
+        }
+    }
+
     public void switchToCreateAnAccount(ActionEvent event) {
 
         Stage stage;
@@ -23,5 +42,7 @@ public class LoginPageController {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+
     }
 }
